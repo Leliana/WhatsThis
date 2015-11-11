7c9d41f5f841a826356b2398474155db5712a4c8
iroul <liyinggrace@gmail.com>
2015-11-10 20:34:39 -0800

    Fix rgb order :)

--- app/src/main/java/com/happen/it/make/whatisit/MxNetUtils.java
+++ app/src/main/java/com/happen/it/make/whatisit/MxNetUtils.java
@@ -24,9 +24,9 @@ public class MxNetUtils {
         float mean_r = WhatsApplication.getMean().get("r");
         for (int i = 0; i < bytes.length; i += 4) {
             int j = i / 4;
-            colors[0 * 224 * 224 + j] = (float)(((int)(bytes[i + 2])) & 0xFF) - mean_b;
+            colors[0 * 224 * 224 + j] = (float)(((int)(bytes[i + 0])) & 0xFF) - mean_r;
             colors[1 * 224 * 224 + j] = (float)(((int)(bytes[i + 1])) & 0xFF) - mean_g;
-            colors[2 * 224 * 224 + j] = (float)(((int)(bytes[i + 0])) & 0xFF) - mean_r;
+            colors[2 * 224 * 224 + j] = (float)(((int)(bytes[i + 2])) & 0xFF) - mean_b;
         }
         Predictor predictor = WhatsApplication.getPredictor();
         predictor.forward("data", colors);
